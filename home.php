cb541c99be89a458a29c021ce05ac3421e4ceb4e
Luis <apenaspara1998@gmail.com>
2019-08-15 15:24:23 +0100

    Some fixes to make the website more mobile friendly

--- home.php
+++ home.php
@@ -1,14 +1,18 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <link rel="stylesheet" href="./css/index.css">
-        <script type="module" src="./js/index.js"></script>
-    </head>
-    <body>
-        <nav>
-            <a class="float" href="#">Home</a>
-            <a class="float" href="#">Void</a>
-            <a class="float" href="#">Corruption</a>
-        </nav>
-    </body>
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/index.css">
+    <script type="module" src="./js/index.js"></script>
+</head>
+
+<body>
+    <nav>
+        <a class="float" href="#">Home</a>
+        <a class="float" href="#">Void</a>
+        <a class="float" href="#">Corruption</a>
+    </nav>
+</body>
+
 </html>
