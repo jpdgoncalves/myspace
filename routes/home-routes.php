4c43215e6ec69b405cfd2f64c3a8aaeb33599576
Luis <apenaspara1998@gmail.com>
2019-08-21 14:52:55 +0100

    More fixes

--- routes/home-routes.php
+++ routes/home-routes.php
@@ -2,7 +2,7 @@
 require "libs/php/routing.php";
 
 add_route("/", HttpMethods::GET, function() {
-    $css_version = "1.0.1";
-    $js_version = "1.0.1";
+    $css_version = "1.0.2";
+    $js_version = "1.0.2";
     include "webpages/home/home.php";
 });
