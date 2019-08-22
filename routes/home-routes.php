df4057557842ee12a1c1bc6398c777376ac324ab
Luis <apenaspara1998@gmail.com>
2019-08-22 14:51:01 +0100

    Small fixes to css

--- routes/home-routes.php
+++ routes/home-routes.php
@@ -2,48 +2,48 @@
 require "libs/php/routing.php";
 
 add_route("/", HttpMethods::GET, function() {
-    $css_version = "1.0.5";
+    $css_version = "1.0.6";
     $js_version = "1.0.3";
     include "webpages/home/home.php";
 });
 
 add_route("/projects/python", HttpMethods::GET, function() {
-    $css_version = "1.0.5";
+    $css_version = "1.0.6";
     $js_version = "1.0.3";
     $project_subject = "python";
     include "webpages/home/projects.php";
 });
 
 add_route("/projects/python/", HttpMethods::GET, function() {
-    $css_version = "1.0.5";
+    $css_version = "1.0.6";
     $js_version = "1.0.3";
     $project_subject = "python";
     include "webpages/home/projects.php";
 });
 
 add_route("/projects/javascript", HttpMethods::GET, function() {
-    $css_version = "1.0.5";
+    $css_version = "1.0.6";
     $js_version = "1.0.3";
     $project_subject = "javascript";
     include "webpages/home/projects.php";
 });
 
 add_route("/projects/javascript/", HttpMethods::GET, function() {
-    $css_version = "1.0.5";
+    $css_version = "1.0.6";
     $js_version = "1.0.3";
     $project_subject = "javascript";
     include "webpages/home/projects.php";
 });
 
 add_route("/projects/java", HttpMethods::GET, function() {
-    $css_version = "1.0.5";
+    $css_version = "1.0.6";
     $js_version = "1.0.3";
     $project_subject = "java";
     include "webpages/home/projects.php";
 });
 
 add_route("/projects/java/", HttpMethods::GET, function() {
-    $css_version = "1.0.5";
+    $css_version = "1.0.6";
     $js_version = "1.0.3";
     $project_subject = "java";
     include "webpages/home/projects.php";
